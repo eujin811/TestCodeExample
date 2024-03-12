838106f62ac5451ff17f6ba82646e8fd3429d194
eujin8@gmail.com <eujin8@gmail.com>
2024-03-12 23:04:41 +0900

    :tada: unitTestExample

---
+++ UnitTestExample/UnitTestExample/UnitTestExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestExampleApp.swift
+//  UnitTestExample
+//
+//  Created by YoujinMac on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
