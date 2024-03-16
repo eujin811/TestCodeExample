0130d0ed971524eec51b0a3943ee61e48533646b
eujin8@gmail.com <eujin8@gmail.com>
2024-03-16 11:31:21 +0900

    :construction: ConverterViewController 생성

---
+++ UnitTestUIKitExample/UnitTestUIKitExample/ConverterViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ConverterViewController.swift
+//  UnitTestUIKitExample
+//
+//  Created by YoujinMac on 3/17/24.
+//
+
+import UIKit
+
+import UIKit
+
+class ConverterViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func setUI() {
+        
+        
+    }
+    
+    private func setConstraints() {
+        
+    }
+}
