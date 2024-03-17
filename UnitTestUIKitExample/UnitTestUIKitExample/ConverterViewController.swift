4e6ed33cd642ac789ebfb1eb6074a55e0ee468ae
eujin8@gmail.com <eujin8@gmail.com>
2024-03-17 17:56:07 +0900

    :construction: Converter ViewController UI

--- UnitTestUIKitExample/UnitTestUIKitExample/ConverterViewController.swift
+++ UnitTestUIKitExample/UnitTestUIKitExample/ConverterViewController.swift
@@ -7,17 +7,49 @@
 
 import UIKit
 
-import UIKit
+import Then
+import SnapKit
 
 class ConverterViewController: UIViewController{
+    private lazy var wrappingView = UIView()
+    private lazy var titleLbl = UILabel().then {
+        $0.text = "아라비안 숫자 -> 로마숫자 변경"
+    }
+    private lazy var arabicNumLbl = UILabel()
+    private lazy var romanNumLbl = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.setUI()
     }
     
     private func setUI() {
+        self.view.addSubview(wrappingView)
         
+        wrappingView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.height.equalTo(400)
+            $0.width.equalTo(300)
+        }
         
+        wrappingView.addSubview(titleLbl)
+        wrappingView.addSubview(arabicNumLbl)
+        wrappingView.addSubview(romanNumLbl)
+        
+        titleLbl.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.centerX.equalToSuperview()
+        }
+        
+        arabicNumLbl.snp.makeConstraints {
+            $0.top.equalTo(titleLbl.snp.bottom).offset(30)
+            $0.centerX.equalToSuperview()
+        }
+        
+        romanNumLbl.snp.makeConstraints {
+            $0.top.equalTo(arabicNumLbl.snp.bottom).offset(30)
+            $0.centerX.equalToSuperview()
+        }
     }
     
     private func setConstraints() {
